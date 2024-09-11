82bb900ed0c9290332868038edb565b68dfd0f94
SATHESHKUMAR S <satheshk.sakthivel@gmail.com>
2024-09-11 13:28:04 +0530

    created create update interface for CourseDetails model

--- Task for MVC/Controllers/CourseDetailsController.cs
+++ Task for MVC/Controllers/CourseDetailsController.cs
@@ -1,40 +1,99 @@
 ﻿using DataAccessLayer;
+using DataAccessLayer.Entity;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-using Task_for_MVC.Models;
 
 namespace Task_for_MVC.Controllers
 {
     public class CourseDetailsController : Controller
     {
-        ICourseDetailsRepository cours = null;
-        public CourseDetailsController(IConfiguration configuration, ICourseDetailsRepository course)
+        ICourseDetailsRepository course = null;
+        public CourseDetailsController(IConfiguration configuration, ICourseDetailsRepository cours)
         {
-            cours = course;
+            course = cours;
         }
-
-
-        public IActionResult List()
+        // GET: CourseDetailsController
+        public ActionResult List()
         {
-            var result = cours.SelectALLStudent();
+            var result = course.SelectALLStudent();
             return View("List",result);
         }
 
-        public IActionResult Privacy()
+        // GET: CourseDetailsController/Details/5
+        public ActionResult Details(int id)
         {
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        // GET: CourseDetailsController/Create
+        public ActionResult Create()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View("Add",new CourseDetails());
+        }
+
+        // POST: CourseDetailsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(CourseDetails cour)
+        {
+            try
+            {
+                course.RegisterUser(cour);
+                return RedirectToAction(nameof(List));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CourseDetailsController/Edit/5
+        public ActionResult Edit(string username)
+        {
+            var edit = course.SelectUserByName(username);
+            return View("update",edit);
+        }
+
+        // POST: CourseDetailsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(CourseDetails cours)
+        {
+            try
+            {
+                course.UpdateUser(cours);
+                return RedirectToAction(nameof(List));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CourseDetailsController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CourseDetailsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
         }
     }
 }
